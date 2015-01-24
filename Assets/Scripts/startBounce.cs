f7c6b14a69a913d14aa07669c41170c1b278c8ac
Mikael E. Wikner <wikner.mikael@gmail.com>
2015-01-24 21:02:11 +0100

    made start game text relatively positioned

--- Assets/Scripts/startBounce.cs
+++ Assets/Scripts/startBounce.cs
@@ -5,16 +5,17 @@ using System.Collections;
 public class startBounce : MonoBehaviour {
 
 	public Text text;
+	float y;
 
 	// Use this for initialization
 	void Start () {
-	
+		y = text.rectTransform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 position = text.rectTransform.position;
-		position.y = 190 + 5 * Mathf.Sin (Time.time);
+		position.y = y + 5 * Mathf.Sin (Time.time);
 		text.rectTransform.position = position;
 	}
 }
