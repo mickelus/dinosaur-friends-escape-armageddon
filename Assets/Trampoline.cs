f98b8c3f426351773d7738404e6c0c577d195629
Oliver Norin <oliver.david.norin@gmail.com>
2015-01-24 20:55:10 +0100

    >:)

---
+++ Assets/Trampoline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (LineRenderer))]
+public class Trampoline : MonoBehaviour {
+
+	public Transform[] segments;
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < segments.Length; i++){
+			GetComponent<LineRenderer>().SetPosition(i, segments[i].position);
+		}
+	}
+}
