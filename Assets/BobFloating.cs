69549c99d70f74dc6e9575d86865ef395c640edf
maxpaj <maximilian.paju@gmail.com>
2015-01-24 16:46:33 +0100

    nytt

---
+++ Assets/BobFloating.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobFloating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(transform.position.x, transform.position.y + 1.0f * Mathf.Sin (Time.time), transform.position.z));
+	}
+}
---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+	public GameObject[] players;
+	public GameObject dialogToShow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool show = false;
+
+		foreach (GameObject child in players)
+		{
+			float distance = Mathf.Abs(transform.position.x - child.transform.position.x);
+
+			if(distance < 2.0f) {
+				show = true;
+			}
+		}
+		dialogToShow.SetActive(show);
+	}
+}
