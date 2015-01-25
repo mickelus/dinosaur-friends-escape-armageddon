0c57e53dcc53365e079fc0264e8c3b774c46745b
Mikael E. Wikner <wikner.mikael@gmail.com>
2015-01-25 05:59:34 +0100

    started work on death screen

---
+++ Assets/Scripts/deathScreenScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets.CrossPlatformInput;
+
+public class deathScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("update");
+		if (CrossPlatformInputManager.GetButtonDown ("Player Start")) {
+			print ("Player Start");
+			Application.LoadLevel("startScreen");
+		}
+	}
+}
