6c6dae5460d13f9a3e740f0bafdb7ddc29fbd784
Mikael E. Wikner <wikner.mikael@gmail.com>
2015-01-25 15:02:13 +0100

    final commit!

---
+++ Assets/Scripts/creditsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class creditsScript : MonoBehaviour {
+
+	public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		print ("asdasd");
+		Vector3 pos = canvas.transform.position;
+		pos.y = Screen.height;
+		print (pos.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = canvas.transform.position;
+		pos.y -= Time.deltaTime*40;
+		canvas.transform.position = pos;
+	}
+}
---
+++ Assets/endScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Application.LoadLevel ("credits");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
